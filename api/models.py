22b232bf89212439e2ffcaea63285faf810159d5
Andre Arsyan <andrearsyanj@gmail.com>
2023-09-24 16:52:15 -0400

    Fix request model

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Item(models.Model):
     name = models.CharField(max_length=200)
     qty = models.IntegerField()
-    vendor = models.CharField(max_length=200,null=True)
+    vendor = models.CharField(max_length=200,null=True,blank=True)
 
     def __str__(self):
         return self.name
@@ -20,12 +20,12 @@ class Status(models.Model):
 
 class Request(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    request_date = models.DateField(null=True)
-    order_date = models.DateField(null=True)
-    received_date = models.DateField(null=True)
-    approved_date = models.DateField(null=True)
+    request_date = models.DateField(null=True,blank=True)
+    order_date = models.DateField(null=True,blank=True)
+    received_date = models.DateField(null=True,blank=True)
+    approved_date = models.DateField(null=True,blank=True)
     status = models.ForeignKey(Status, on_delete=models.CASCADE)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,)
 
     def __str__(self):
-        return self.id
+        return self.item.name
