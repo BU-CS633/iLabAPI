06554e7ae4d9add13bf2530c38b4f2b027529aed
Andre Arsyan <andrearsyanj@gmail.com>
2023-09-24 03:01:24 -0400

    Change dotenv lib

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 class Item(models.Model):
     item_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.item_name
--- iLabAPI/settings.py
+++ iLabAPI/settings.py
@@ -12,11 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import environ
+from dotenv import load_dotenv
 
 # Get .env
-env = environ.Env()
-environ.Env.read_env()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,11 +86,11 @@ WSGI_APPLICATION = "iLabAPI.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
