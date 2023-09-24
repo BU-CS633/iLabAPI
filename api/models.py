5743d7729bba9e4a31513d321d76ac2a50b47c0b
Andre Arsyan <andrearsyanj@gmail.com>
2023-09-24 16:42:32 -0400

    Migrate initial tables

--- api/models.py
+++ api/models.py
@@ -1,7 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Item(models.Model):
-    item_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    qty = models.IntegerField()
+    vendor = models.CharField(max_length=200,null=True)
 
     def __str__(self):
-        return self.item_name
+        return self.name
+
+
+class Status(models.Model):
+    description = models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.description
+
+
+class Request(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    request_date = models.DateField(null=True)
+    order_date = models.DateField(null=True)
+    received_date = models.DateField(null=True)
+    approved_date = models.DateField(null=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
