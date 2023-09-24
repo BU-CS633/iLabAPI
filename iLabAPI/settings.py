1ead3a47db5bcba569c7e05385b4db0e4b85efbe
Andre Arsyan <andrearsyanj@gmail.com>
2023-09-24 04:12:24 -0400

    Enable CORS

--- iLabAPI/settings.py
+++ iLabAPI/settings.py
@@ -47,9 +47,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -146,3 +148,6 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Allow cors header
+CORS_ALLOW_ALL_ORIGINS = True
