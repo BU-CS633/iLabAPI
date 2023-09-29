2fad0f9d033b06561e0aec6b4b87e5fc997a66d8
your_username <fchen12@student.gsu.edu>
2023-09-28 22:36:39 -0400

    Create a file for item_detail for a better organization

---
+++ api/item_detail.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse, Http404
+from .models import Item
+
+
+def item_detail(request, item_id):
+    try:
+        item = Item.objects.get(pk=item_id)
+        data = {
+            "id": item.id,
+            "name": item.name,
+            "qty": item.qty,
+            "vendor": item.vendor,
+        }
+        return JsonResponse(data)
+    except Item.DoesNotExist:
+        raise Http404("Item not found")
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from . import views
+from . import item_detail
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("item/<int:item_id>/", views.item_detail, name="item_detail"),
+    path("item/<int:item_id>/", item_detail.item_detail, name="item_detail"),
 ]
