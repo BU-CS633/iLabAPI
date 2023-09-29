7b928b0a268c37232887e3d87be464108edddfb5
your_username <fchen12@student.gsu.edu>
2023-09-28 22:14:17 -0400

    Create [API] Create endpoint /api/item/<item_id> to get item detail

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("item/<int:item_id>/", views.item_detail, name="item_detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.http import JsonResponse, Http404
+from .models import Item
 
 
 def index(request):
     return HttpResponse("Hello CS633")
+
+
+def item_detail(request, item_id):
+    try:
+        item = Item.objects.get(pk=item_id)
+        data = {
+            "id": item.id,
+            "name": item.name,
+            "qty": item.qty,
+            "vendor": item.vendor,
+        }
+        return JsonResponse(data)
+    except Item.DoesNotExist:
+        raise Http404("Item not found")
