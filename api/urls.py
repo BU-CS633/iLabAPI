85663008d7662ad4e16c3ed8e19b3ec6e76e64fe
Zhiling <lzhiling@bu.edu>
2023-10-20 23:31:26 -0400

    Modify endpoint  `/api/item/<item_id>` to be able to update item detail using POST method

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,6 @@ from . import item_detail
 urlpatterns = [
     path("", views.index, name="index"),
     path("item/<int:item_id>/", item_detail.item_detail, name="item_detail"),
+    path("api/item/<int:item_id>/", views.update_item, name="update_item"),
+
 ]
